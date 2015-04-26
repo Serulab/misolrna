76dbbd9546169dfae4ebc40993a13e495f6861a9
Sebastian Bassi <sbassi@genesdigitales.com>
2015-04-26 23:18:26 +0000

    server cfg file

--- settings/server.py
+++ settings/server.py
@@ -1,11 +1,12 @@
 import os
 from common import *
 
-STATIC_ROOT = '/Users/sbassi/projects/serulab/misolrna/static/'
+#STATIC_ROOT = '/Users/sbassi/projects/serulab/misolrna/static/'
+STATIC_ROOT = '/var/www/misolrna.org.v2/misolrna/static/'
 
 BASE_URL = 'misolrna.org'
 
-ROOT_DIR = '/Users/sbassi/projects/serulab/misolrna/'
+ROOT_DIR = '/var/www/misolrna.org.v2/misolrna/'
 DB_NAME = os.path.join(ROOT_DIR,'mirna20.db')
 
 #STATIC_FS = '/var/www/%s/htdocs/img/static/'%BASE_URL
@@ -13,7 +14,10 @@ DB_NAME = os.path.join(ROOT_DIR,'mirna20.db')
 STATIC_URL = 'static'
 
 # BLAST path
-BLASTN_EXE = '/usr/local/ncbi/blast/bin/blastn'
+BLASTN_EXE = '/var/www/misolrna.org.v2/blast/ncbi-blast-2.2.23+/bin/blastn'
 
 # BLAST DBs path
-BLASTDB_PATH = '/Users/sbassi/projects/serulab/misolrna/blastdbs/'
+BLASTDB_PATH = '/var/www/misolrna.org.v2/misolrna/blastdbs/'
+
+# VENV
+VENVS = '/var/www/misolrna.org.v2/venvs/misolrnav2/lib/python2.7/site-packages/'
